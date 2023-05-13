2e8778dbffb4336e2d9eaa300233a844a2911080
mohammad mohammadi <mohammadim48@gmail.com>
2023-05-13 13:11:39 +0330

    install tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,6 +20,9 @@ export default function Home() {
             Get started by editing&nbsp;
             <code className={styles.code}>pages/index.tsx</code>
           </p>
+
+          
+          <h1 className='text-red-500 text-2xl font-bold'>mohammad</h1>
           <div>
             <a
               href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
